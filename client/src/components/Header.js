9bc76598011eea63c3f72b4859fa363a5191d515
Vlad <chepurnojv@gmail.com>
2021-09-22 19:06:27 +0300

    Redirecting a User on Auth Header Content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className='nav-warapper'>
           <a className='left brand-logo'>Emaily</a>
-          <ul className='right'>
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className='right'>{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
